3acf4b42e8e4308705fc962694e61ca2e2b95f17
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-23 13:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value})
   }
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  Object.assigh(obj, {[key]: value})
+  return obj
+}
