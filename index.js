55daa90532a561285d90e30eaaa172176a17c3f7
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-23 13:10:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value})
   }
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
-  Object.assigh(obj, {[key]: value})
+  Object.assign(obj, {[key]: value})
   return obj
 }
